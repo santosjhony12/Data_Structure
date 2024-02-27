9ca3273645b9fa00eeeb6585e6b269c63be6bf6f
Jhony Santos <123211025+santosjhony12@users.noreply.github.com>
2024-02-27 15:27:38 -0300

    Update aula2.java

--- codes/aula2.java
+++ codes/aula2.java
@@ -3,9 +3,7 @@ import java.util.List;
 
 public class aula2 {
     public static void main(String[] args) {
-        int[] array = {7,3,5,8,10,2,20,30,8,9,10,11,12};
-        aula2.imprimirAlfabeto();
-        System.out.println(aula2.maiorTamanhoDeUmaSequenciaV2(array));
+    
     }
     public static void imprimirAlfabeto(){
         for (int ch = 97; ch <= 122; ch++ ){
@@ -13,38 +11,6 @@ public class aula2 {
         }
 
     }
-
-    public static int maiorTamanhoDeUmaSequencia(int[] vetor){
-        if(vetor.length == 1){
-            return 1;
-        } else if (vetor.length == 0) {
-            return 0;
-        }
-        List<Integer> listSim = new LinkedList<>();
-        List<Integer> listNao = new LinkedList<>();
-
-        for(int i = 0; i < vetor.length-1; i++){
-            if(vetor[i]<vetor[i+1]){
-                listSim.add(vetor[i]);
-            }else{
-                listSim.add(vetor[i]);
-                if(listSim.size() > listNao.size()){
-                    listNao = new LinkedList<>(listSim);
-                }
-                listSim.clear();
-            }
-        }
-        if(vetor[vetor.length-1] > vetor[vetor.length-2]){
-            listSim.add(vetor[vetor.length-1]);
-        }
-
-        if(listSim.size()>listNao.size()){
-            return listSim.size();
-        }else{
-            return listNao.size();
-        }
-    }
-
 
     public static int maiorTamanhoDeUmaSequenciaV2(int[] vetor){
         if(vetor.length == 1){
@@ -69,10 +35,18 @@ public class aula2 {
         return (tms>aux) ? tms : aux;
     }
 
-    public static int[] uniaoOrdenada(int[] a, int[] b){
-        int tamanho = a.length + b.length;
-        a[]
-        return;
-
+    public static ArrayList<Integer> uniaoOrdenada(ArrayList<Integer> vetorA, ArrayList<Integer> vetorB){
+        int indexA = 0;
+        int indexB = 0;
+        while (indexB < vetorB.size()) {
+            if (indexA >= vetorA.size() || vetorB.get(indexB) < vetorA.get(indexA)) {
+                vetorA.add(indexA, vetorB.get(indexB));
+                indexB++;
+            } else if (vetorB.get(indexB).equals(vetorA.get(indexA))) {
+                indexB++;
+            }
+            indexA++;
+        }
+        return vetorA;
     }
 }
