4f75d277ac22017e569324a77c3c0f1be5155d9f
Jhony Santos <123211025+santosjhony12@users.noreply.github.com>
2024-02-28 16:28:35 -0300

    Update Ordenacao.java

--- codes/Ordenacao.java
+++ codes/Ordenacao.java
@@ -1,15 +1,15 @@
 public class Ordenacao{
-public static int[] insert_sort(int[] array){
-	for(int i = 1; i < array.length; i ++){
-		int key = array[i];
-		int index = i - 1;
-		
-		while(index >= 0 && array[index] > key){
-			array[index + 1] = array[index];
-			index -= 1;
+	public static int[] insert_sort(int[] array){
+		for(int i = 1; i < array.length; i ++){
+			int key = array[i];
+			int index = i - 1;
+			
+			while(index >= 0 && array[index] > key){
+				array[index + 1] = array[index];
+				index -= 1;
+			}
+			array[index + 1] = key
 		}
-		array[index + 1] = key
+		return array;
 	}
-	return array;
-}
 }
