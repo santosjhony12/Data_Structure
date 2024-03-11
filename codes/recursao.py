e245840f97d8ac18e92e6c05117f59fe92157f75
santosjhony12 <santosdesouzajhony@gmail.com>
2024-03-11 20:53:28 -0300

    Recursão

--- codes/recursao.py
+++ codes/recursao.py
@@ -7,7 +7,7 @@ def fatorial(n):
 
 
 def fibonnaci(n):
-    if n == 0 or n ==1:
+    if n >=0:
         return n
     else:
         return fibonnaci(n-1) + fibonnaci(n-2)
@@ -39,4 +39,43 @@ def toBinary(decimal):
     else:
         toBinary(decimal//2)
         print(decimal%2, end='')
-print(toBinary(10))
+#print(toBinary(10))
+
+def soma(vetor):
+    size = len(vetor)
+    if (size == 0):
+        return 0
+    else:
+        return vetor[size-1]+soma(vetor[:size-1])
+    
+lista = [1, 2, 3, 4, 4,4,4,4,4]  
+#print(soma(lista))
+def numerosNaturais(n):
+    print(n)
+    if n>0:
+        numerosNaturais(n-1)
+
+#numerosNaturais(9)
+
+
+def quantElementosList(lista, num):
+    soma = 0
+    size = len(lista)
+    if(size == 1):
+        if(num == lista[size-1]):
+            return 1
+        else:
+            return 0
+    else:
+        if(num == lista[size-1]):
+            soma+=1
+        soma += quantElementosList(lista[:-1], num)
+        return soma
+    
+def somaElementosAnteriores(n):
+    if (n<=0):
+        return 0
+    else:
+        return n+somaElementosAnteriores(n-1)
+    
+print(somaElementosAnteriores(10))
